043760e3776d3f850463a60587c4aa09729ddc50
Rayk Kretzschmar <rkre75@gmail.com>
2023-05-20 00:29:36 +0200

    update

--- src/FewoVerwaltung.py
+++ src/FewoVerwaltung.py
@@ -3,27 +3,6 @@ import csv
 import docx
 import datetime
 
-def replace_text(replacements, doc_path="Rechnungsvorlage.docx"):
-    doc = Document(doc_path)
-
-    # Go through each paragraph in the document
-    for p in doc.paragraphs:
-        inline = p.runs
-        for i in range(len(inline)):
-            for old_text, new_text in replacements.items():
-                if old_text in inline[i].text:
-                    text = inline[i].text.replace(old_text, new_text)
-                    inline[i].text = text
-
-    datum = datetime.datetime.now()
-    tag = datum.strftime("%d")
-    monat = datum.strftime("%m")
-    jahr = datum.strftime("%Y")
-
-    # Save the document
-    file_path = f"Rechnung{tag}{monat}{jahr}.docx"
-    doc.save(file_path)
-
 class NeuerKundeDialog(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
@@ -113,6 +92,27 @@ class AuswahlDialog(tk.Toplevel):
 
         # Then close the dialog
         self.destroy()
+
+    def replace_text(replacements, doc_path="Rechnungsvorlage.docx"):
+        doc = Document(doc_path)
+
+        # Go through each paragraph in the document
+        for p in doc.paragraphs:
+            inline = p.runs
+            for i in range(len(inline)):
+                for old_text, new_text in replacements.items():
+                    if old_text in inline[i].text:
+                        text = inline[i].text.replace(old_text, new_text)
+                        inline[i].text = text
+
+        datum = datetime.datetime.now()
+        tag = datum.strftime("%d")
+        monat = datum.strftime("%m")
+        jahr = datum.strftime("%Y")
+
+        # Save the document
+        file_path = f"Rechnung{tag}{monat}{jahr}.docx"
+        doc.save(file_path)
 
 
 class VerwaltungListener(tk.Tk):
