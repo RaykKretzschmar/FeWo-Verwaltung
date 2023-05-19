2b47e6dffde8ec594ea659af0fae92875ab7f08d
Rayk Kretzschmar <rkre75@gmail.com>
2023-05-20 00:16:56 +0200

    update Textersetzung.py

--- src/Textersetzung.py
+++ src/Textersetzung.py
@@ -1,21 +1,23 @@
 from docx import Document
+import datetime
 
-def replace_text(old_text, new_text):
-    doc = Document("Rechnungsvorlage.docx")  # Fügen Sie hier den Pfad Ihrer Datei ein
+def replace_text(replacements, doc_path="Rechnungsvorlage.docx"):
+    doc = Document(doc_path)
 
-    # Geht durch jeden Absatz im Dokument
+    # Go through each paragraph in the document
     for p in doc.paragraphs:
-        # Wenn der alte Text im Absatz ist, ersetzen Sie ihn
-        if old_text in p.text:
-            inline = p.runs
-            # Loop added to work with runs (continuous sequence of characters with same style)
-            for i in range(len(inline)):
-                print(inline[i].text)
+        inline = p.runs
+        for i in range(len(inline)):
+            for old_text, new_text in replacements.items():
                 if old_text in inline[i].text:
                     text = inline[i].text.replace(old_text, new_text)
                     inline[i].text = text
 
-    # Speichert das Dokument
-    doc.save("neu_Rechnungsvorlage.docx")
+    datum = datetime.datetime.now()
+    tag = datum.strftime("%d")
+    monat = datum.strftime("%m")
+    jahr = datum.strftime("%Y")
 
-replace_text(old_text="Straße", new_text="Dornburger Str.")  # Ersetzt "alter Text" durch "neuer Text"
+    # Save the document
+    file_path = f"Rechnung{tag}{monat}{jahr}.docx"
+    doc.save(file_path)
