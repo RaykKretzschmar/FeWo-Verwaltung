8c2911750f64d1dc04e813b6142bd7b216d0ef35
Rayk Kretzschmar <rkre75@gmail.com>
2023-05-21 20:14:37 +0200

    unique customer number; correct text substitution

--- src/FewoVerwaltung.py
+++ src/FewoVerwaltung.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import csv
 import Textersetzung
+import datetime
 
 class NeuerKundeDialog(tk.Toplevel):
     def __init__(self, parent):
@@ -9,7 +10,6 @@ class NeuerKundeDialog(tk.Toplevel):
         self.title("Neuer Kunde")
 
         self.labels = {
-            "Kürzel": tk.StringVar(),
             "Anrede": tk.StringVar(),
             "Vorname": tk.StringVar(),
             "Nachname": tk.StringVar(),
@@ -17,7 +17,7 @@ class NeuerKundeDialog(tk.Toplevel):
             "Postleitzahl": tk.StringVar(),
             "Straße": tk.StringVar(),
             "Hausnummer": tk.StringVar(),
-            "Kundennummer": tk.StringVar(),
+            "Kundennummer": tk.StringVar(value=self.generate_kundennummer()),
         }
 
         for i, (k, v) in enumerate(self.labels.items()):
@@ -35,15 +35,18 @@ class NeuerKundeDialog(tk.Toplevel):
             writer.writerow([v.get() for v in self.labels.values()])
         self.destroy()
 
+    def generate_kundennummer(self):
+        return datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+
 
 class AuswahlDialog(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.title("Kunde auswählen")
+        self.title("Kunden")
         self.configure()
 
-        self.labels = ["Kürzel", "Anrede", "Vorname", "Nachname", "Stadt", "PLZ", "Straße", "Hausnummer", "Kundennummer"]
+        self.labels = ["Anrede", "Vorname", "Nachname", "Stadt", "PLZ", "Straße", "Hausnummer", "Kundennummer"]
         self.checkboxes = []
 
         for i, label in enumerate(self.labels):
@@ -76,16 +79,19 @@ class AuswahlDialog(tk.Toplevel):
 class EingabeDialog(tk.Toplevel):
     def __init__(self, parent, data):
         super().__init__(parent)
+        self.parent = parent
 
         self.title("Rechnung erstellen")
 
         # data should be a dictionary
         for i, (k, v) in enumerate(data.items()):
-            tk.Label(self, text=f"{k} : {v}", anchor="e").grid(row=i, column=0, sticky="e")
+            tk.Label(self, text=f"{k} :", anchor="e").grid(row=i, column=0, sticky="e")
+            tk.Label(self, text=v, anchor="w").grid(row=i, column=1, sticky="w")
+            self.grid_rowconfigure(i, weight=1)  # make the row expandable
 
         self.new_labels = {
             "Datum": tk.StringVar(),
-            "Rechnungsnummer": tk.StringVar(),
+            "Rechnungsnummer": tk.StringVar(), # Please don't enter "/". This will break the invoice creation process.
             "Anreisedatum": tk.StringVar(),
             "Abreisedatum": tk.StringVar(),
             "Name der Ferienwohnung": tk.StringVar(),
@@ -95,19 +101,46 @@ class EingabeDialog(tk.Toplevel):
         for j, (k, v) in enumerate(self.new_labels.items(), start=i+1):
             tk.Label(self, text=f"{k} : ", anchor="e").grid(row=j, column=0, sticky="e")
             tk.Entry(self, textvariable=v, width=30).grid(row=j, column=1, sticky="ew")
+            self.grid_rowconfigure(j, weight=1)  # make the row expandable
 
-        tk.Button(self, text="Speichern", command=self.save).grid(row=j+1, column=0)
-        tk.Button(self, text="Abbrechen", command=self.destroy).grid(row=j+1, column=1)
+        tk.Button(self, text="Rechnung erstellen", command=self.invoice).grid(row=j+1, column=1, sticky="ew")
+        tk.Button(self, text="Abbrechen", command=self.destroy).grid(row=j+1, column=0, sticky="ew")
 
+        self.grid_columnconfigure(0, weight=1)  # make the first column expandable
         self.grid_columnconfigure(1, weight=1)  # make the second column expandable
 
-    def save(self):
-        # Merge selected data and new data
-        all_data = {**self.parent.selected_data, **{k: v.get() for k, v in self.new_labels.items()}}
+    def invoice(self):
+        # Format for date input
+        date_format = "%d.%m.%Y"
+        tax_percent = 7
 
-        with open('Kunden.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(all_data.values())
+        # Convert the dates to datetime objects
+        anreisedatum = datetime.datetime.strptime(self.new_labels['Anreisedatum'].get(), date_format)
+        abreisedatum = datetime.datetime.strptime(self.new_labels['Abreisedatum'].get(), date_format)
+
+        number_of_nights = (abreisedatum - anreisedatum).days
+        price_per_night = float(self.new_labels['Preis pro Nacht'].get().replace(',', '.'))
+        price_for_all_nights = number_of_nights*price_per_night
+        total_price = price_for_all_nights
+        tax_amount = tax_percent*total_price/100
+        net_amount = total_price-tax_amount
+
+        # Merge selected data and new data
+        all_data = {**self.parent.selected_data, 
+                    **{k: v.get() for k, v in self.new_labels.items()}, 
+                    'AnzahlDerÜbernachtungen': str(number_of_nights), 
+                    'PpN': f'{price_per_night:.2f}'.replace('.', ','),
+                    'NdFeWo': self.new_labels['Name der Ferienwohnung'].get(),
+                    'ÜNKosten': f'{price_for_all_nights:.2f}'.replace('.', ','),
+                    'GesamtBetrag': f'{total_price:.2f}'.replace('.', ','),
+                    'MwstBetrag': f'{tax_amount:.2f}'.replace('.', ','),
+                    'NettoBetrag': f'{net_amount:.2f}'.replace('.', ','),
+                    }
+
+        # replace text in .docx
+        invoice_number = self.new_labels['Rechnungsnummer'].get()
+        Textersetzung.replace_text(all_data, f'Rechnung{invoice_number}.docx')
+
         self.destroy()
 
 
@@ -117,20 +150,25 @@ class VerwaltungListener(tk.Tk):
 
         self.title("Fewo Kundenverwaltung by Rayk Kretzschmar")
         self.geometry("800x600")
-        self.configure(bg='white')
+        self.configure()
+
+        # Add a headline
+        self.headline = tk.Label(self, text="FeWo-Verwaltung von Rayk Kretzschmar", font=('Arial', 20))
+        self.headline.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
         # Adjust font, colors, and padding of the buttons
-        self.neuerKundeButton = tk.Button(self, text="Neuer Kunde", command=self.open_neuerKundeDialog, font=('Arial', 14), bg='skyblue', fg='black', padx=10, pady=10)
-        self.rechnungButton = tk.Button(self, text="Rechnung erstellen", command=self.open_auswahlDialog, font=('Arial', 14), bg='skyblue', fg='black', padx=10, pady=10)
+        self.neuerKundeButton = tk.Button(self, text="Neuer Kunde", command=self.open_neuerKundeDialog, font=('Arial', 14), bg='skyblue', fg='black', padx=5, pady=5)
+        self.rechnungButton = tk.Button(self, text="Rechnung erstellen", command=self.open_auswahlDialog, font=('Arial', 14), bg='skyblue', fg='black', padx=5, pady=5)
 
         # Use grid layout manager and add some margins
-        self.neuerKundeButton.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
-        self.rechnungButton.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
+        self.neuerKundeButton.grid(row=1, column=0, padx=10, pady=10)
+        self.rechnungButton.grid(row=1, column=1, padx=10, pady=10)
 
         # Configure the columns and row to expand when the window is resized
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)
 
     def open_neuerKundeDialog(self):
         NeuerKundeDialog(self)
--- src/Textersetzung.py
+++ src/Textersetzung.py
@@ -1,23 +1,36 @@
 from docx import Document
 import datetime
 
-def replace_text(replacements, doc_path="Rechnungsvorlage.docx"):
+def replace_text(replacements, save_path, doc_path="Rechnungsvorlage.docx"):
     doc = Document(doc_path)
 
     # Go through each paragraph in the document
     for p in doc.paragraphs:
-        inline = p.runs
-        for i in range(len(inline)):
-            for old_text, new_text in replacements.items():
-                if old_text in inline[i].text:
-                    text = inline[i].text.replace(old_text, new_text)
-                    inline[i].text = text
+        full_text = p.text
+        for old_text, new_text in replacements.items():
+            full_text = full_text.replace(old_text, new_text)
+        # Clear the paragraph and replace with new text
+        p.clear()
+        p.add_run(full_text)
+    
+    # Go through each table in the document
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                full_text = cell.text
+                for old_text, new_text in replacements.items():
+                    full_text = full_text.replace(old_text, new_text)
+                # Clear the cell and replace with new text
+                cell.text = full_text
 
-    datum = datetime.datetime.now()
-    tag = datum.strftime("%d")
-    monat = datum.strftime("%m")
-    jahr = datum.strftime("%Y")
-
+    if not save_path:
+        datum = datetime.datetime.now()
+        tag = datum.strftime("%d")
+        monat = datum.strftime("%m")
+        jahr = datum.strftime("%Y")
+        save_path = f"Rechnung{tag}{monat}{jahr}.docx"
+    
     # Save the document
-    file_path = f"Rechnung{tag}{monat}{jahr}.docx"
-    doc.save(file_path)
+    doc.save(save_path.replace('/', ''))
+    
+    
