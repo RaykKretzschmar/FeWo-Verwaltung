37ee5e169339b820c8782245b341d0b3b898b1fd
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2025-08-24 23:00:32 +0200

    Update settings to include static files configuration and modify home view to render template

--- fewo_web/fewo/fewo/views.py
+++ fewo_web/fewo/fewo/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Fewo</h1>")
+    return render(request, "home.html")
--- fewo_web/fewo/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.http import HttpResponse
-# from django.urls import path
-
-# # ...existing code...
-
-# urlpatterns = [
-#     # ...existing code...
-#     path("", lambda request: HttpResponse("Welcome to FeWo Verwaltung!")),
-# ]
