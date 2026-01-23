99971b8691d0a71ffcf09d751eec8e2b537ef323
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2026-01-23 02:18:11 +0100

    fix: script

--- start_server.bat
+++ start_server.bat
@@ -1,16 +1,25 @@
 @echo off
 TITLE FeWo Verwaltung Server
 
+REM --- CONFIGURATION ---
+REM We force the script to use the Anaconda Python found in your logs
+SET "PYTHON_EXEC=c:\users\rkre7\anaconda3\python.exe"
+REM ---------------------
+
 echo Starting FeWo Verwaltung...
+echo Using Python at: "%PYTHON_EXEC%"
 echo.
 
 REM Ensure we are in the script's directory
 cd /d "%~dp0"
 
-echo Checking/Installing dependencies...
-pip install -r requirements.txt
+echo Checking dependencies...
+REM We use the specific python executable to run pip
+"%PYTHON_EXEC%" -m pip install -r requirements.txt
 if %errorlevel% neq 0 (
-    echo Error installing dependencies. Please ensure Python is installed and added to PATH.
+    echo.
+    echo [ERROR] Could not install dependencies.
+    echo Please check if the path in 'PYTHON_EXEC' matches your Anaconda installation.
     pause
     exit /b
 )
@@ -25,6 +34,6 @@ start http://127.0.0.1:8000
 
 echo.
 echo Starting Django Server...
-python manage.py runserver
+"%PYTHON_EXEC%" manage.py runserver
 
 pause
